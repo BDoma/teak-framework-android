35c6c10a5a02a2c3e1dc86f95eb220d637cbfe7e
Domonkos <domaherfli@gmail.com>
2021-01-21 01:05:22 +0100

    Implement Toolbar MenuItemListener for BottomAppBar

---
+++ src/main/java/com/tea/kotlin/android/actions/MenuItemClickListener.kt
@@ -0,0 +1,22 @@
+package com.tea.kotlin.android.actions
+
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.tea.kotlin.android.common.ActionLifecycle
+
+class MenuItemClickListener<Msg>(
+    lifecycle: ActionLifecycle<Msg>,
+    private val transform: ((Int) -> Msg?)
+) : Action<Msg>(lifecycle), Toolbar.OnMenuItemClickListener {
+    override fun onMenuItemClick(menuItem: MenuItem?): Boolean {
+        menuItem?.let {
+            transform(menuItem.itemId)?.let {
+                dispatch(it)
+                return true
+            }
+        }
+        return false
+    }
+
+
+}
